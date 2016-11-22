8e3c90b230f6c4eb7b26c6ff81bf007f8897e6ec
R. S. Doiel <rsdoiel@gmail.com>
2016-11-22 10:38:14 -0800

    merged in additional page related functions

--- tmplfuncs.go
+++ tmplfuncs.go
@@ -20,7 +20,12 @@
 package tmplfuncs
 
 import (
+	"encoding/json"
 	"fmt"
+	"go/doc"
+	"io/ioutil"
+	"log"
+	"net/url"
 	"strconv"
 	"strings"
 	"text/template"
@@ -120,6 +125,104 @@ var (
 			}
 			return dt.Format(time.RFC822)
 		},
+		"datefmt": func(dt, outputFmtYMD, outputFmtYM, outputFmtY string) string {
+			var (
+				inputFmt  string
+				outputFmt string
+			)
+			// NOTE: Date input formats can be YYYY, YYYY-MM and YYYY,
+			// we need to define output formats for each
+			switch {
+			case len(dt) == 4:
+				inputFmt = "2006"
+				outputFmt = outputFmtY
+			case len(dt) > 4 && len(dt) <= 7:
+				inputFmt = "2006-01"
+				outputFmt = outputFmtYM
+			default:
+				inputFmt = "2006-01-02"
+				outputFmt = outputFmtYMD
+			}
+			//intputFmt: 2006-01-02
+			//outputfmt: Jan _2, 2006
+			d, err := time.Parse(inputFmt, dt)
+			if err != nil {
+				return fmt.Sprintf("%s, %s", dt, err.Error())
+			}
+			return d.Format(outputFmt)
+		},
+	}
+
+	Page = template.FuncMap{
+		"nl2p": func(s string) string {
+			return strings.Replace(strings.Replace(s, "\n\n", "<p>", -1), "\n", "<br />", -1)
+		},
+		"contains": func(s, substring string) bool {
+			return strings.Contains(s, substring)
+		},
+		"title": func(s string) string {
+			return strings.Title(s)
+		},
+		"add": func(a, b int) int {
+			return a + b
+		},
+		"sub": func(a, b int) int {
+			return a - b
+		},
+		"arraylength": func(a []string) int {
+			return len(a)
+		},
+		"mapsize": func(m map[string]string) int {
+			return len(m)
+		},
+		"prevPage": func(from, size, max int) int {
+			next := from - size
+			if next < 0 {
+				return 0
+			}
+			return next
+		},
+		"nextPage": func(from, size, max int) int {
+			next := from + size
+			if next > max {
+				return from
+			}
+			return next
+		},
+		"getType": func(t interface{}) string {
+			switch tp := t.(type) {
+			default:
+				return fmt.Sprintf("%T", tp)
+			}
+		},
+		"asList": func(li []interface{}, sep string) string {
+			var l []string
+			for _, item := range li {
+				l = append(l, fmt.Sprintf("%s", item))
+			}
+			return strings.Join(l, sep)
+		},
+		"synopsis": func(s string) string {
+			return doc.Synopsis(s)
+		},
+		"encodeURIComponent": func(s string) string {
+			u, err := url.Parse(s)
+			if err != nil {
+				log.Printf("Bad encoding request: %s, %s\n", s, err)
+				return ""
+			}
+			return strings.Replace(u.String(), "&", "%26", -1)
+		},
+		"stringify": func(data interface{}, prettyPrint bool) string {
+			if prettyPrint == true {
+				if buf, err := json.MarshalIndent(data, "", "\t"); err == nil {
+					return string(buf)
+				}
+			} else if buf, err := json.Marshal(data); err == nil {
+				return string(buf)
+			}
+			return ""
+		},
 	}
 )
 
@@ -157,4 +260,27 @@ func Join(maps ...template.FuncMap) template.FuncMap {
 		}
 	}
 	return result
+}
+
+// AssembleTemplate support a very simple template setup of an outer HTML file with a content include
+// used by caitpage and caitserver
+func AssembleTemplate(htmlFilename, includeFilename string) (*template.Template, error) {
+	htmlTmpl, err := ioutil.ReadFile(htmlFilename)
+	if err != nil {
+		return nil, fmt.Errorf("Can't read html template %s, %s", htmlFilename, err)
+	}
+	includeTmpl, err := ioutil.ReadFile(includeFilename)
+	if err != nil {
+		return nil, fmt.Errorf("Can't read included template %s, %s", includeFilename, err)
+	}
+	return template.New(includeFilename).Funcs(Join(Time, Page)).Parse(fmt.Sprintf(`{{ define "content" }}%s{{ end }}%s`, includeTmpl, htmlTmpl))
+}
+
+// Template generate a template struct with Time and Page functions attach.
+func Template(filename string) (*template.Template, error) {
+	src, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("Can't read template %s, %s", filename, err)
+	}
+	return template.New(filename).Funcs(Join(Time, Page)).Parse(string(src))
 }
