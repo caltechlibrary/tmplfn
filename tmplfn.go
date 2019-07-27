abfa7c66bae79b3c359c561657ae332314f75a1b
R. S. Doiel <rsdoiel@gmail.com>
2019-07-27 09:49:08 -0700

    bump version number with removal of embedded markdown support

--- tmplfn.go
+++ tmplfn.go
@@ -41,7 +41,7 @@ import (
 
 var (
 	// Version of tmplfn package
-	Version = `v0.0.20`
+	Version = `v0.0.21`
 
 	// Time provides a common set of time/date related functions for use in text/template or html/template
 	Time = template.FuncMap{
